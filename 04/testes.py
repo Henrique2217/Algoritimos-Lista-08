7e07ecc575c610c2e91394cf4bd0e6301c5738b1
Henrique Trentin <htrentin17@gmail.com>
2024-05-28 22:25:24 -0300

    04

--- 04/testes.py
+++ 04/testes.py
@@ -27,7 +27,16 @@ def test_verificar_resistencia_true():
 
 
 
-def test_calcular_grau_aco_10():
+def test_calcular_grau_aco_10(mocker):
+    mock_validar_conteudo_carbono = mocker.patch("funcoes.verificar_conteudo_carbono")
+    mock_validar_conteudo_carbono.return_value = True
+
+    mock_verificar_dureza_rockwell = mocker.patch("funcoes.verificar_dureza_rockwell")
+    mock_verificar_dureza_rockwell.return_value = True
+
+    mock_verificar_resistencia_tracao = mocker.patch("funcoes.verificar_resistencia_tracao")
+    mock_verificar_resistencia_tracao.return_value = True
+
     grau_aco = calcular_grau_aco(5 , 75, 85000)
     assert grau_aco == 10
 
